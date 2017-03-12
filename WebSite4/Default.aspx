f8f27eacbeb6d09a54a9b1e663934b39ba8ff1fd
alecmartinSEA <alecmartin28@gmail.com>
2017-03-12 16:03:57 -0700

    Add files via upload
    Assignment2

---
+++ WebSite4/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+    <div>
+        <asp:calendar ID="Calendar1" runat="server"></asp:calendar>
+        </br>
+        <asp:button runat="server" text="Check For Events" OnClick="CheckEvents_Click" />
+        <h3>EVENTS ON THIS DAY</h3>
+        <asp:Label ID="label1" runat="server" text=""></asp:Label>
+    
+    </div>
+    </form>
+</body>
+</html>
+        </h3>
+        
---
+++ WebSite4/Default.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    
+    
+    //create 2d arrray
+    string[,] events = new string[6, 2];
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        //add things to 2d array
+        //0 = date 1 = the event name
+        events[0, 0] = "1/26/2017";
+        events[0, 1] = "4/4 Thursday";
+        events[1, 0] = "1/29/2017";
+        events[1, 1] = "Birthday";
+        events[2, 0] = "1/31/2017";
+        events[2, 1] = "Taco Tuesday";
+        events[3, 0] = "1/31/2017";
+        events[3, 1] = "dnb tuesdays";
+        events[4, 0] = "2/2/2017";
+        events[4, 1] = "Grime show";
+        events[5, 0] = "2/6/2017";
+        events[5, 1] = "Spirit Days";
+
+        
+       
+
+    }
+
+    protected void CheckEvents_Click(object sender, EventArgs e)
+    {
+        CompareDate();
+
+    }
+
+    protected void CompareDate()
+    {
+        //Get the Date for the Calendar
+        String calendarDate = Calendar1.SelectedDate.ToShortDateString();
+        label1.Text = "";
+        //looping through the events 
+
+        //looping through length of events 
+        for (int i = 0; i < events.GetLength(0); i++)
+        {
+            //if calendar matches events date 
+            if (calendarDate.Equals(events[i, 0]))
+            {
+                //print event
+                label1.Text += events[i, 1] + "<br/>";
+
+            }
+
+       }
+
+    }
+    
+    
+}
