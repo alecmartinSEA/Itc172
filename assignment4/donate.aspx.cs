e6720efe76a5e84091b1309a8eab79c188887f98
alecmartinSEA <alecmartin28@gmail.com>
2017-03-12 18:00:01 -0700

    Add files via upload

---
+++ assignment4/donate.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Donate : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        if (Session["userKey"] == null)
+            Response.Redirect("Default.aspx");
+    }
+
+    protected void DonateButton1_Click(object sender, EventArgs e)
+    {
+        Community_AssistEntities cae = new Community_AssistEntities();
+        
+        int key = (int)Session["userKey"];
+
+        
+        Donation d = new Donation();
+        d.DonationAmount = decimal.Parse(DonationTextBox.Text);
+        d.DonationDate = DateTime.Now;
+        d.PersonKey = key;
+
+        
+        cae.Donations.Add(d);
+        
+        cae.SaveChanges();
+
+        Response.Redirect("Donations.Aspx");
+    }
+}
